c1030c261793fb1f73450d9da80f1639de7a4d9d
shu-illy <sh.illy.9117@gmail.com>
2022-03-19 06:31:18 +0900

    leson21

--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./TaskItem.module.css";
 import firebase from "firebase/app";
 import { FormControl, TextField, Grid, ListItem } from "@material-ui/core";
 import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
@@ -36,10 +37,10 @@ const TaskItem: React.FC<PROPS> = (props) => {
           }
         />
       </Grid>
-      <button onClick={editTask}>
+      <button className={styles.taskitem__icon} onClick={editTask}>
         <EditOutlinedIcon />
       </button>
-      <button onClick={deleteTask}>
+      <button className={styles.taskitem__icon} onClick={deleteTask}>
         <DeleteOutlineOutlinedIcon />
       </button>
     </ListItem>
